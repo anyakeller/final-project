20d49e699d298d18e56b50237571d7a9aa376eb7
Anya L Keller <kelleranyal@gmail.com>
2020-01-10 22:21:02 -0500

    Added left and right buttons

--- client/src/components/AnimationDiv/index.js
+++ client/src/components/AnimationDiv/index.js
@@ -5,25 +5,36 @@ import {useTransition, animated} from 'react-spring';
 import './style.css';
 
 export default function AnimationDiv(props) {
+  const [direction, setDirection] = useState(1); //1 or -1
   const [index, set] = useState(0);
-  const onClick = useCallback(
-    () => set(state => (state + 1) % props.contacts.length),
-    []
-  );
+  const onClickRight = () => {
+    setDirection(1);
+    return set(state => (state + 1) % props.contacts.length);
+  };
+  const onClickLeft = () => {
+    setDirection(-1);
+    set(state => {
+      if (state === 0) return props.contacts.length - 1;
+      else return (state - 1) % props.contacts.length;
+    });
+  };
   const transitions = useTransition(props.contacts[index], item => item._id, {
-    from: {opacity: 0, transform: 'translate3d(100%,0,0)'},
+    from: {opacity: 0, transform: `translate3d(${100 * direction}%,0,0)`},
     enter: {opacity: 1, transform: 'translate3d(0%,0,0)'},
-    leave: {opacity: 0, transform: 'translate3d(-50%,0,0)'}
+    leave: {opacity: 0, transform: `translate3d(${-50 * direction}%,0,0)`}
   });
 
-
   return (
-    <div className="simple-trans-main" onClick={onClick}>
-      {transitions.map(({item, props, key}) => (
-        <animated.div key={key} className="bg" style={{...props}}>
-          <ContactCard key={item._id} contactData={item}></ContactCard>
-        </animated.div>
-      ))}
+    <div>
+      <button onClick={onClickLeft}>Left</button>
+      <div className="simple-trans-main">
+        {transitions.map(({item, props, key}) => (
+          <animated.div key={key} className="bg" style={{...props}}>
+            <ContactCard key={item._id} contactData={item}></ContactCard>
+          </animated.div>
+        ))}
+      </div>
+      <button onClick={onClickRight}>Right</button>
     </div>
   );
 }
