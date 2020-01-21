a8e4a0a79d2a7598c80c47a7bb69ce89e552fca6
Anya L Keller <kelleranyal@gmail.com>
2020-01-20 21:09:12 -0500

    didn't do much again

--- client/src/components/ContactCard/index.js
+++ client/src/components/ContactCard/index.js
@@ -1,32 +1,77 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import DeleteBtn from '../../components/DeleteBtn';
-import { ListItem, List } from '../List';
-import { useTransition, animated } from 'react-spring';
+import {ListItem, List} from '../List';
+import {useTransition, animated} from 'react-spring';
+import API from '../../utils/API';
 
 class ContactCard extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      name_first: this.props.contactData.name_first,
+      name_last: this.props.contactData.name_last,
+      quickref: this.props.contactData.quickref,
+      meeting_info: this.props.contactData.meeting_info
+    };
   }
+
+  updateContact = (id, contactData) => {
+    API.saveContact(id, contactData);
+  };
 
   render() {
     return (
       <List>
         <ListItem id="contact-card">
           <DeleteBtn onClick={e => this.props.del(this.props._id)} />
-          <h4
-            style={{
-              borderBottom: ".1rem solid pink",
-              color: "navy",
-              fontFamily: "Shadows Into Light Two, cursive",
-              marginBottom: "1rem",
-              marginTop: ".5rem",
-              paddingLeft: ".5rem",
-              width: "95%"
-            }}>
-            {this.props.contactData.name_first} {this.props.contactData.name_last}
-          </h4>
-          <span className="contact-label">PERSONAL DETAILS</span><p> {this.props.contactData.quickref}</p>
-          <span className="contact-label">MEETING NOTES</span><p> {this.props.contactData.meeting_info}</p>
+          <form>
+            <h4
+              style={{
+                borderBottom: '.1rem solid pink',
+                color: 'navy',
+                fontFamily: 'Shadows Into Light Two, cursive',
+                marginBottom: '1rem',
+                marginTop: '.5rem',
+                paddingLeft: '.5rem',
+                width: '95%'
+              }}>
+              <input
+                style={{
+                  border: 'none'
+                }}
+                type="text"
+                name="asfd"
+                value={this.state.name_first + ' ' + this.state.name_last}
+                disabled
+              />
+            </h4>
+            <span className="contact-label">PERSONAL DETAILS</span>
+            <p>
+              <input
+                style={{
+                  border: 'none',
+                  borderBottom: '0.01rem solid lightskyblue'
+                }}
+                type="text"
+                name="asfd"
+                value={this.state.quickref}
+                disabled
+              />
+            </p>
+            <span className="contact-label">MEETING NOTES</span>
+            <p>
+              <input
+                style={{
+                  border: 'none',
+                  borderBottom: '0.01rem solid lightskyblue'
+                }}
+                type="text"
+                name="asfd"
+                value={this.state.meeting_info}
+                disabled
+              />
+            </p>
+          </form>
         </ListItem>
       </List>
     );
